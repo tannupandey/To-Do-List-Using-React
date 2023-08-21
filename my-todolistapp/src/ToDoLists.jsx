8d7fdc432222c61c50e1c94bf82cd2d69b62d10a
tannupandey <tannuppandey2005@gmail.com>
2023-08-21 23:50:12 +0530

    React To-do List

---
+++ my-todolistapp/src/ToDoLists.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const ToDoLists = (props) =>{
+    
+const deleteItems = (props) => {
+    const deleteItems = () =>{
+        console.log("deleted");
+    }
+}
+
+    return (
+    <>
+    <div className='todo_style'>
+    <i className="fa fa-times"
+    aria-hidden="true"
+    onClick= {() =>{
+        props.onSelect(props.id);
+    }}
+     />
+    
+    <li>{props.text}</li>
+    </div>
+    </>
+   
+    );
+     
+};
+export default ToDoLists;
